1158f3437bc60381691163b7a335694ee5b117f8
Steve Bishop <xipooo@gmail.com>
2018-02-13 19:25:17 -0800

    Added actions that take in parameters for GET and POST

--- WozUCoreDemo/Controllers/HomeController.cs
+++ WozUCoreDemo/Controllers/HomeController.cs
@@ -3,13 +3,25 @@ using Microsoft.AspNetCore.Mvc;
 namespace WozUCoreDemo.Controllers
 {
 
-// Use Route Attribute with placeholders to define endpoint
     [Route("api/[controller]/[action]")]
     public class HomeController : Controller
     {
         [HttpGet]
         public string Index() {
+
             return "Home";
+        }
+
+        // Add get that receives value from URL
+        [HttpGet("{value}")]
+        public string Index(string value){
+            return value;
+        }
+        
+        // Add post that receives value from body of request
+        [HttpPost]
+        public string Post([FromBody]string value){
+            return "Posted Value:" + value;
         }
     }
 }
