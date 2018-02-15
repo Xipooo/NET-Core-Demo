884638e765446df1d5cac56d59023b08a2762d6a
Steve Bishop <xipooo@gmail.com>
2018-02-15 15:01:15 -0800

    Added Post, Put, and Delete examples

--- WozUCoreDemo/Controllers/CustomerController.cs
+++ WozUCoreDemo/Controllers/CustomerController.cs
@@ -2,38 +2,71 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WozUCoreDemo.Models;
+
 namespace WozUCoreDemo.Controllers
 {
-    // Add new Customer Controller for more parameter demonstration
-    // Route does not indicate actions will be on this controller.  
-    // HTTP actions will be used instead to determine which method to call.
     [Route("api/[controller]")]
     public class CustomerController : Controller
     {
-        // Internal variable to hold a list of customers
         private List<Customer> customers;
         public CustomerController()
         {
-            // Create the initial values for customers List
             customers = new List<Customer>{
                 new Customer { Id = 1, FirstName = "Steve", LastName = "Bishop", Email = "steve.bishop@codercamps.com" },
                 new Customer { Id = 2, FirstName = "Sandra", LastName = "Marine", Email = "sandy_mar19@home.net" }
             };
         }
 
-        // GET action to return all customers
         [HttpGet]
         public List<Customer> GetAllCustomers()
         {
             return customers;
         }
 
-        // GET action to return a single customer
         [HttpGet("{id}")]
         public Customer GetCustomer(int id)
         {
             var customer = customers.FirstOrDefault(cust => cust.Id == id);
             return customer;
+        }
+
+        // Use POST action to create new Customer
+        [HttpPost]
+        public Customer AddCustomer([FromBody]Customer newCustomer)
+        {
+            if (newCustomer != null)
+            {
+                customers.Add(newCustomer);
+            }
+            return newCustomer;
+        }
+
+        // Use PUT action to update a customer
+        [HttpPut("{id}")]
+        public Customer UpdateCustomer(int id, [FromBody]Customer updatedCustomer)
+        {
+            var customer = customers.FirstOrDefault(cust => cust.Id == id);
+            if (customer != null)
+            {
+                customer.FirstName = updatedCustomer.FirstName;
+                customer.LastName = updatedCustomer.LastName;
+                customer.Email = updatedCustomer.Email;
+                return customer;
+            }
+            return null;
+        }
+
+        // Use DELETE action to delete customer
+        [HttpDelete("{id}")]
+        public Customer DeleteCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault(cust => cust.Id == id);
+            if (customer != null)
+            {
+                customers.Remove(customer);
+                return customer;
+            }
+            return null;
         }
     }
 }
