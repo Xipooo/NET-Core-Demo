64ac2d4bc419c0a9565ea1996dff36d3a3afd9d0
Steve Bishop <xipooo@gmail.com>
2018-02-16 16:24:23 -0800

    Added In Memory Database

--- WozUCoreDemo/Controllers/CustomerController.cs
+++ WozUCoreDemo/Controllers/CustomerController.cs
@@ -8,28 +8,26 @@ namespace WozUCoreDemo.Controllers
     [Route("api/[controller]")]
     public class CustomerController : Controller
     {
-        private List<Customer> customers;
-        public CustomerController()
+        // Dependency injection to get the Database Context
+        private WozUContext _context;
+        public CustomerController(WozUContext context)
         {
-            customers = new List<Customer>{
-                new Customer { Id = 1, FirstName = "Steve", LastName = "Bishop", Email = "steve.bishop@codercamps.com" },
-                new Customer { Id = 2, FirstName = "Sandra", LastName = "Marine", Email = "sandy_mar19@home.net" }
-            };
+            _context = context;
         }
 
-        // Convert return object to IActionResult and use base functions to return appropriate response
+        // Update to use the Database Context
         [HttpGet]
         public IActionResult GetAllCustomers()
         {
+            var customers = _context.Customers.ToList();
             return base.Ok(customers);
         }
 
-        // Can use Json function to return an object
-        // Update function to check if customer was found
+        // Use the Database Context
         [HttpGet("{id}")]
         public IActionResult GetCustomer(int id)
         {
-            var customer = customers.FirstOrDefault(cust => cust.Id == id);
+            var customer = _context.Customers.FirstOrDefault(cust => cust.Id == id);
             if (customer != null)
             {
                 return base.Json(customer);
@@ -37,32 +35,31 @@ namespace WozUCoreDemo.Controllers
             return base.NotFound();
         }
 
-        // Update function to return proper result
+        // Use the Database Context
         [HttpPost]
         public IActionResult AddCustomer([FromBody]Customer newCustomer)
         {
-            // Use a try catch in case there is a problem adding the object to storage
             if (newCustomer != null)
             {
                 try
                 {
-                    customers.Add(newCustomer);
+                    _context.Customers.Add(newCustomer);
+                    _context.SaveChanges();
                     return base.Created($"/{newCustomer.Id}", newCustomer);
                 }
                 catch (System.Exception ex)
                 {
-                    // Can specify status code to return and a message in the body of the response
                     return base.StatusCode(500, ex.Message);
                 }
             }
             return base.BadRequest();
         }
 
-        // Update method for appropriate response
+        // Use the Database Context
         [HttpPut("{id}")]
         public IActionResult UpdateCustomer(int id, [FromBody]Customer updatedCustomer)
         {
-            var customer = customers.FirstOrDefault(cust => cust.Id == id);
+            var customer = _context.Customers.FirstOrDefault(cust => cust.Id == id);
             if (customer != null)
             {
                 try
@@ -70,26 +67,27 @@ namespace WozUCoreDemo.Controllers
                     customer.FirstName = updatedCustomer.FirstName;
                     customer.LastName = updatedCustomer.LastName;
                     customer.Email = updatedCustomer.Email;
-                    // Request worked but no content to return (Ok, but without a body)
+                    _context.Update(customer);
+                    _context.SaveChanges();
                     return base.NoContent();
                 }
                 catch (System.Exception ex)
                 {
-                    // Error is probably because of bad data
-                    return base.BadRequest();
+                    return base.BadRequest(ex.Message);
                 }
             }
             return base.NotFound();
         }
 
-        // Update to send appropriate response
+        // Use the Database Context
         [HttpDelete("{id}")]
         public IActionResult DeleteCustomer(int id)
         {
-            var customer = customers.FirstOrDefault(cust => cust.Id == id);
+            var customer = _context.Customers.FirstOrDefault(cust => cust.Id == id);
             if (customer != null)
             {
-                customers.Remove(customer);
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
                 return base.NoContent();
             }
             return base.NotFound();
